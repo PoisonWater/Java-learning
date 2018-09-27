3fb25838b2a794ec088201273dd91e75a886cd48
PoisonWater <117010178@link.cuhk.edu.cn>
2018-09-27 14:42:47 +0800

    BoolSet

---
+++ BooleanSet.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+public class BooleanSet {
+
+    private boolean[] data;
+    private int size;
+    private HashSet set;
+
+    public BooleanSet(int size, HashSet set) {
+        this.size = size;
+        this.set = set;
+    }
+
+
+}
